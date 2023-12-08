bcdb4b85c25a0a77058681f9866dd1641cc541b1
besantej <83356167+besantej@users.noreply.github.com>
2023-12-08 15:12:18 +0100

    Update kidneyexchange.py

--- python/kidneyexchange.py
+++ python/kidneyexchange.py
@@ -20,6 +20,8 @@ class Instance:
     def __init__(self, filepath=None):
         self.nodes = []
         self.edges = []
+        self.cycleListe = []
+        self.M=[]
         self.maximum_length = 1
         if filepath is not None:
             with open(filepath) as json_file:
@@ -31,6 +33,13 @@ class Instance:
                         data["edge_weights"])
                 for (node_1_id, node_2_id, weight) in edges:
                     self.add_edge(node_1_id, node_2_id, weight)
+    
+    def get_vertices(self) -> set[int]:
+        vertices : set[int] = set()
+        for edge in self.edges:
+            vertices.add(edge.node_1_id)
+            vertices.add(edge.node_2_id)
+        return vertices
 
     def add_node(self):
         node = Node()
@@ -48,6 +57,25 @@ class Instance:
         while max(node_id_1, node_id_2) >= len(self.nodes):
             self.add_node()
         self.nodes[node_id_1].edges.append((edge.id, node_id_2))
+    
+    #TODO Le j° vertex appartient au i°cycle
+    def matrice(self,CycleList):
+        m = [[0 for _ in range(len(CycleList))] for _ in range(len(self.get_vertices()))]
+        for cycle_no, cycle in enumerate(CycleList):
+            for edge_id in cycle:
+                u = self.edges[edge_id].node_2_id
+                v = self.edges[edge_id].node_1_id
+                m[u][cycle_no] = 1
+                m[v][cycle_no] = 1
+        return m
+    #TODO END
+    
+    def add_edge_2(self, edge):
+        self.edges.append(edge)
+        while max(edge.node_id_1, edge.node_id_2) >= len(self.nodes):
+            self.add_node()
+        self.nodes[edge.node_id_1].edges.append((edge.id, edge.node_id_2))
+    #TODO END
 
     def write(self, filepath):
         data = {"maximum_cycle_length": self.maximum_cycle_length,
@@ -104,40 +132,207 @@ class Instance:
             print(f"Weight: {weight}")
             return (is_feasible, weight)
 
+def bellman_ford(instance : Instance) -> list[list[int]]:
+    nb_vertices = len(instance.get_vertices())
+    vertices = range(nb_vertices)
+    tab_bellmanford : list[list[int]] = [[0 for _ in vertices] for __ in vertices]
+
+    def update_tab(instance : Instance, tab : list[list[int]], i : int, v : int) -> int|None:
+        """
+            Calcule l_{i+1}(v) = min( l_{i}(v), l_{i}(u) + c_{uv} )
+        """
+        best_val : int = tab[i][v]
+        best_vertex : int|None = None
+        for edge in instance.edges:
+            if (edge.node_1_id == v):
+                u = edge.node_2_id
+                if tab[i][u] + edge.weight <= best_val:
+                    best_val = tab[i][u] + edge.weight
+                    best_vertex = u
+        
+        tab[i+1][v] = best_val
+        return best_vertex
+
+    # Initialisation
+    for v in range(1, nb_vertices):
+        tab_bellmanford[0][v] = float("inf")
+    
+    # Boucle principale
+    backtrack_dict : dict[tuple[int, int], int|None] = {}
+    for i in range(nb_vertices - 1):
+        for v in range(nb_vertices):
+            backtrack_dict[(i + 1, v)] = update_tab(instance, tab_bellmanford, i, v)
+    
+    potential_sols : list[list[int]] = []
+    for v in vertices:
+        visited : list[int] = [v]
+        i = nb_vertices - 1
+        w = v
+        has_been_added = False
+        while i > 0:
+            u = backtrack_dict[(i, w)]
+            if u is None:
+                pass
+
+            elif u not in visited:
+                # u est un sommet pas encore visité
+                visited.append(u)
+                w = u
+            else:
+                # u a déjà été visité par le passé
+                u_pos = visited.index(u)
+                visited = visited[u_pos:] + [u]
+                visited.reverse()
+                has_been_added = True
+                potential_sols.append(visited)
+                w = u
+                break
+            i -= 1
+        
+        if not has_been_added and len(visited) > 1:
+            can_construct_cycle = False
+            u = visited[-1]
+            v = visited[0]
+            for edge in instance.edges:
+                if (edge.node_1_id == u) and (edge.node_2_id == v):
+                    can_construct_cycle = True
+                    break
+            
+            if not can_construct_cycle:
+                continue
+            
+            visited.append(visited[0])
+            visited.reverse()
+            
+            total_weight = 0
+            for i in range(len(visited) - 1):
+                u = visited[i]
+                v = visited[i+1]
+                for edge in instance.edges:
+                    if (edge.node_1_id == v) and (edge.node_2_id == u):
+                        total_weight += edge.weight
+            if total_weight < 0:
+                potential_sols.append(visited)
+    if len(potential_sols) == 0:
+        return []
+    # Keeping only solutions with <= K arcs
+    all_vertex_cycles = [cycle for cycle in potential_sols if len(cycle) <= instance.maximum_length+1 and len(cycle) >= 3]
+    all_vertex_cycles.sort(key = lambda cycle : len(cycle))
+
+    if len(all_vertex_cycles) == 0:
+        return []
+    
+    all_cycles : list[list[int]] = []
+    for cycle_found in all_vertex_cycles:
+        solution : list[int] =  []
+        for i in range(len(cycle_found) - 1):
+            u, v = cycle_found[i], cycle_found[i+1]
+            for edge in instance.edges:
+                if (edge.node_1_id == v) and (edge.node_2_id == u):
+                    solution.append(edge.id)
+            i=0
+        all_cycles.append(solution)
+    return all_cycles
+
+def dynamic_programming(instance : Instance) -> list[int]:
+    """
+        Takes an instance as an input, returns the list of
+        edge_id corresponding to the negative cycle found
+    """
+    all_cycles = bellman_ford(instance)
+    if len(all_cycles) == 0:
+        return []
+    return all_cycles[0]
 
 class PricingSolver:
 
     def __init__(self, instance):
         self.instance = instance
         # TODO START
+        self.vertices_used = None
         # TODO END
 
     def initialize_pricing(self, columns, fixed_columns):
         # TODO START
-        pass
+        self.vertices_used = [0] * len(self.instance.get_vertices())
+        for column_id, column_value in fixed_columns:
+            column = columns[column_id]
+            for row_index, row_coefficient in zip(column.row_indices,
+                                                  column.row_coefficients):
+                self.vertices_used[row_index] += (column_value*row_coefficient)            
         # TODO END
 
     def solve_pricing(self, duals):
         # Build subproblem instance.
         # TODO START
-        # TODO END
+        cout_max=-1
+        cycle_max = 0
+        for j, cycle in enumerate(instance.cycleListe):
+            
+            profit = sum([edge.weight for edge in self.instance.edges if edge.id in cycle])
+            for vertex in range(len(instance.get_vertices())):
+                profit -= duals[vertex]*self.instance.M[vertex][j]
+            
+            if profit <= 0:
+                continue
+            if profit > cout_max:
+                cout_max=profit
+                cycle_max=cycle
+        if cout_max == -1:
+            return []
+        
 
-        # Solve subproblem instance.
-        # TODO START
         # TODO END
 
         # Retrieve column.
         column = columngenerationsolverpy.Column()
         # TODO START
+        column.objective_coefficient = 0
+        
+        
+        for edge in self.instance.edges:
+            if edge.id in cycle_max:
+            #if self.instance.edges[j] > 0:
+                column.row_indices.append(edge.node_1_id)
+                column.row_coefficients.append(1)
+                column.objective_coefficient += edge.weight
         # TODO END
-
         return [column]
 
 
-def get_parameters(instance):
+def get_parameters(instance : Instance):
     # TODO START
-    number_of_constraints = None
+    number_of_constraints = len(instance.get_vertices()) #number of vertices
     p = columngenerationsolverpy.Parameters(number_of_constraints)
+    # Objective sense.
+    p.objective_sense = "max"
+    # Column bounds.
+    p.column_lower_bound = 0
+    p.column_upper_bound = 1
+    # Row bounds.
+    for edge in instance.edges:
+        edge.weight *= -1
+
+    cycleList : list[list[int]] = bellman_ford(instance)
+    instance.cycleListe =cycleList
+    m = instance.matrice(cycleList)
+    instance.M=m    
+    
+    for edge in instance.edges:
+        edge.weight *= -1
+
+    
+
+    for vertex in range(number_of_constraints):
+        p.row_lower_bounds[vertex] = 0
+        p.row_upper_bounds[vertex] = 1
+        #coefficients
+        p.row_coefficient_lower_bounds[vertex] = min(m[vertex])
+        p.row_coefficient_upper_bounds[vertex] = max(m[vertex])
+
+    # Dummy column objective coefficient.
+    p.dummy_column_objective_coefficient = -1
+
     # TODO END
     # Pricing solver.
     p.pricing_solver = PricingSolver(instance)
@@ -148,7 +343,12 @@ def to_solution(columns, fixed_columns):
     solution = {'cycles': []}
     for column, value in fixed_columns:
         # TODO START
-        pass
+        if value > 0:
+            s = []
+            for index, coef in zip(column.row_indices, column.row_coefficients):
+                if coef > 0:
+                    s.append(index)
+            solution["cycles"].append(s)
         # TODO END
     return solution
 
